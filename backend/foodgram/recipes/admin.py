6e805ef213cedac88d18f63219a47612fe6a6979
Andrey Ponomarev <ponomarev.a.p@gmail.com>
2023-04-15 12:46:28 +0300

    admin display update

--- backend/foodgram/recipes/admin.py
+++ backend/foodgram/recipes/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
+from django.contrib.admin import TabularInline
 
-from .models import Tag, Ingredient, Recipe, Favorite, Cart
+from .models import Tag, Ingredient, Recipe, Favorite, Cart, IngredientRecipe
+
+
+class IngredientInline(TabularInline):
+    """Вспомогательный класс для отображения ингредиентов в модели рецептов."""
+    model = IngredientRecipe
 
 
 @admin.register(Tag)
@@ -12,14 +18,17 @@ class TagAdmin(admin.ModelAdmin):
 @admin.register(Ingredient)
 class IngredientAdmin(admin.ModelAdmin):
     """Административная панель ингридиентов."""
-    list_display = ('name', 'measurement_unit')
-    list_filter = ('name',)
+    list_display = ('pk', 'name', 'measurement_unit')
+    list_editable = ('name', 'measurement_unit',)
+    list_filter = ('measurement_unit',)
+    search_fields = ('name',)
 
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
     """Административная панель рецептов."""
     list_display = ('name', 'author')
+    inlines = (IngredientInline,)
 
 
 @admin.register(Favorite)
--- backend/foodgram/recipes/management/commands/load_ingredients.py
+++ backend/foodgram/recipes/management/commands/load_ingredients.py
@@ -8,11 +8,11 @@ class Command(BaseCommand):
     help = 'Loads the ingredients to database from csv file'
 
     def handle(self, *args, **options):
-        with open('data/ingredients.csv', encoding='utf-8') as file:
-            reader = csv.DictReader(file)
+        with open('../../data/ingredients.csv', 'r', encoding='utf-8') as file:
+            reader = csv.reader(file)
             try:
                 Ingredient.objects.bulk_create(
-                    Ingredient(**items) for items in reader
+                    Ingredient(name=item[0], measurement_unit=item[1]) for item in reader
                 )
             except Exception as e:
                 return f'Возникла ошибка при импорте из csv-файла: {e}'
