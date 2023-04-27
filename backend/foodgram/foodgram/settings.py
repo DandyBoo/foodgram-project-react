c8a6ceaf561032382611c5d1c2d68be235b030b9
Andrey Ponomarev <ponomarev.a.p@gmail.com>
2023-04-27 15:58:49 +0300

    edit docker-compose.yml and token get.env

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+# SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-$=_2x_gsw5z&^@@*rwn#@djbtmtj)nf#3m&l&l$^-esk(ch0@k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
