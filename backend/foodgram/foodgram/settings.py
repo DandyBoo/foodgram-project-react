c86355ab2a18f7d2d7d4953b24280391bb990034
Andrey Ponomarev <ponomarev.a.p@gmail.com>
2023-04-28 11:03:59 +0300

    update settings.py

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -109,7 +109,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'foodgram2',
         'USER': 'postgres',
-        'PASSWORD': 'postgres',
+        'PASSWORD': '4332',
         'HOST': 'db',
         'PORT': '5432'
     }
