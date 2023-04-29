fb722088262d821e037f106ef7252db90beb672f
Andrey Ponomarev <ponomarev.a.p@gmail.com>
2023-04-29 19:15:48 +0300

    update views.py

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -112,7 +112,7 @@ class RecipeViewSet(ModelViewSet, CreateAndDeleteRelatedMixin):
         return FileResponse(
             create_pdf(ingredients),
             as_attachment=True,
-            filename='shopping_cart.pdf')
+            filename='shopping_cart_2.pdf')
 
     @action(methods=['post'], detail=True)
     def favorite(self, request, pk):
